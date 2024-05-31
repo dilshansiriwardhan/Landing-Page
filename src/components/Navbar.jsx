236f15360242749abc2ec3e008d6915af821237b
Dila <dilshansiriwardhan@gmail.com>
2024-06-01 02:44:22 +0530

    Navbar Done for Desktop resolution

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,46 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+import { navItems } from "../constants";
 import logo from '../assets/logo.png';
 import React from 'react';
 
 const Navbar = () => {
+
+  const [ mobileDrawerOpen , setMobileDrawerOpen ] = useState(false);
+
+  const toggleNavbar = () => {
+    setMobileDrawerOpen(!mobileDrawerOpen);
+  };
+
   return (
+
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
         <div className="container px-4 mx-auto relative lg:text-sm">
             <div className="flex justify-between items-center">
 
                 <div className="flex items-center flex-shrink-0">
-                    <img src={logo} alt="logo" className="h-10 mr-2" />
+                    <img src={logo} alt="logo" className="h-8 mr-2" />
                 </div>
-                <ul className="hidden lg:flex ml-14 space-x-12"></ul>
-                <div className="hidden lg:flex justify-center space-x-12 items-center"></div>
+
+                <ul className="hidden lg:flex ml-14 space-x-12">
+                  {navItems.map((item, index) => (
+                    <li key={index}>
+                      <a href={item.href}>{item.label}</a>
+                    </li>
+                  ))}
+                </ul>
+
+                <div className="hidden lg:flex justify-center space-x-4 items-center">
+                  <a href="#" className="py-1 px-3 border rounded-full">
+                    Sign In
+                  </a>
+                  <a
+                    href="#"
+                    className="bg-gradient-to-r from-indigo-500 to-purple-500 py-1.5 px-3 rounded-full"
+                  >
+                    Create an account
+                  </a>
+                </div>
 
             </div>
         </div>
